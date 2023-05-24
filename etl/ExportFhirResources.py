1a414340ab72d68e46249209af0091d0d23a04ed
piotrszul <piotrszul>
2023-05-24 00:24:35 +0000

    Adding comments and finalising the ExportFhirResources  step

--- etl/ExportFhirResources.py
+++ etl/ExportFhirResources.py
@@ -1,31 +1,46 @@
 # Databricks notebook source
+"""
+Bulk export specified FHIR resources from the provided FHIR server and save the data in the 
+specified directory of a file/object store.
+
+:param FHIR_ENDPOINT_URL: the URL to the FHIR API endpoing with bulk export capabilites
+:param DESTINATION_URL: the URL to the dirctory to save the exported resource files
+:param EXPORT_RESOURCES: coma spearated list of resource names to export
+"""
+
 dbutils.widgets.text('FHIR_ENDPOINT_URL', 'https://bulk-data.smarthealthit.org/eyJlcnIiOiIiLCJwYWdlIjoxMDAwMCwiZHVyIjoxMCwidGx0IjoxNSwibSI6MSwic3R1Ijo0LCJkZWwiOjB9/fhir')
 dbutils.widgets.text('DESTINATION_URL', 'dbfs:/tmp/fhir-export-01')
 dbutils.widgets.text('EXPORT_RESOURCES', 'Patient,Condition,Observation,Immunization')
-
-# COMMAND ----------
 
 FHIR_ENDPOINT_URL = dbutils.widgets.get('FHIR_ENDPOINT_URL')
 DESTINATION_URL = dbutils.widgets.get('DESTINATION_URL')
 EXPORT_RESOURCES = dbutils.widgets.get('EXPORT_RESOURCES').split(',')
 
-print(f"""Exporting 
+print(f"""Exporting: 
  resources: {EXPORT_RESOURCES}
  from: `{FHIR_ENDPOINT_URL}`
  to: `{DESTINATION_URL}`
 """)
 
-
 # COMMAND ----------
+
+# 
+# Run bulk export 
+#
 
 import requests
 import time
 import urllib
 from os import path
 
-
 def bulk_export(fhir_url, resources):
     """
+    Performs the bulk export of the specified resources from the given FHIR server. 
+    Initiates the builk export and waits for its completion.
+
+    :param fhir_url: the FHIR API endpoing URL
+    :param resources: the list of named of the resources to export 
+    :return: the final bulk export response
     """
     url = path.join(fhir_url, '$export')
     # Set the required headers for the FHIR bulk export API
@@ -72,17 +87,34 @@ def bulk_export(fhir_url, resources):
         raise Exception(f'Error in kick-off request: {response.status_code}')
 
 
-# COMMAND ----------
 
 export_response = bulk_export(FHIR_ENDPOINT_URL, EXPORT_RESOURCES)
+
+# DEBUG: bulk export response JSON
 print(f"""Server side export sucessful with response:
 {export_response}""")
 
+
 # COMMAND ----------
+
+#
+# Download exported files
+#
 
 from tempfile import TemporaryDirectory
 
-def dowload_export(export_response, destination_url):
+def dowload_export(export_response, destination_url, clean = True):
+    """
+    Downloads the resuls for a bulk export operation to the specified directory.
+
+    :param export_response: the response from a successful bulk export operation
+    :param destination_url: the URL to the directory to save the exported files in
+    :param clean: whether existing files in the specified directory should be removed  
+    """
+
+    if clean:
+        dbutils.fs.rm(destination_url, True)
+
     output = export_response.get('output', [])
     index = 0
     with TemporaryDirectory() as tmpdirname:
@@ -91,13 +123,17 @@ def dowload_export(export_response, destination_url):
             input_type = entry['type']
             filename = path.basename(input_url)
             output_path = path.join(tmpdirname, filename)
-            print(f'Downloading: {input_url} to: {output_path}\n')
             urllib.request.urlretrieve(input_url, output_path)
             temp_url= 'file://' + output_path
             output_url = path.join(destination_url, f'{input_type}.{index:04d}.ndjson')
-            print(f'Moving: {temp_url} to: {output_url}\n')    
             dbutils.fs.mv(temp_url, output_url)
             index = index + 1
 
-dbutils.fs.rm(DESTINATION_URL, True)
 dowload_export(export_response, DESTINATION_URL)
+
+#DEBUG: List downloaded files and their contents
+print("Downloaded files:")
+files = dbutils.fs.ls(DESTINATION_URL)
+for f in files: 
+    print("`%s`: %s" % (f.path, dbutils.fs.head(f.path, 100)))
+
