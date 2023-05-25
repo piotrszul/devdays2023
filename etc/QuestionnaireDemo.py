2b573b1a6fe1d3f1c2229fe017998efc8943f20b
piotrszul <piotrszul>
2023-05-25 02:37:46 +0000

    Finalising the Questionnaire Response demo.

--- etc/QuestionnaireDemo.py
+++ etc/QuestionnaireDemo.py
@@ -1,58 +1,147 @@
 # Databricks notebook source
+"""
+Demonstrates how to use Spark python dataset API 
+and fhirpath with the Pathling `extract()` operation
+to navigate nested data types such as `item` in `Questionnaire`.
+"""
+
 from pathling import PathlingContext
-from pathling import Expression as fpe
+from pathling import Expression as exp
 from pyspark.sql.functions import col, explode, explode_outer, first
 
+#
+# Initialize Pathling context that allows up to 5 nesting levels for
+# recursive data types.
+#
 pc = PathlingContext.create(spark, max_nesting_level=5)
 
+#
+# Create a FHIR data sourceo on a direcotory with ndjson files with sample `Questionnaire` 
+# and `QuestionnaireResponse` resources.
+#
+fhir_ds = pc.read.ndjson("s3://pathling-demo/staging/devdays/questionnaire")
+
 # COMMAND ----------
 
-ds = pc.read.ndjson("s3://pathling-demo/staging/devdays/questionnaire")
+# 
+# Obtain the data frame for `Questionnaire` resource.
+#     
+questionnaire_df = fhir_ds.read('Questionnaire')
 
-# COMMAND ----------
+#
+# Progresively unnnest items of each of the questionnaires. 
+# We expect the schema of:
+# 
+# Questionnaire: struct
+#    id: string
+#    item: array[struct]                    // level 0
+#       linkid: string
+#       text: string
+#       item: array[struct]                 // level 1
+#           linkid: string
+#           text: string
+#           item: array[struct]             // level 2
+#               linkid: string
+#               text: string
+#               item: array[struct]         // level 3
+#                   linkid: string
+#                   text: string
+#                   item: array[struct]     // level 4
+#                       linkid: string
+#                       text: string
+#                       item: array[struct] // level 5 (no item here)
+#                           linkid: string
+#                           text: string
 
-questionnaire_ds = ds.read('Questionnaire')
-item_ds = questionnaire_ds\
+# NOTE: We are using pyspark python dataframe API
+# but the same can be achieved in SQL with subqueries.
+#
+
+item_df = questionnaire_df \
     .withColumn('item0', explode_outer('item')) \
     .withColumn('item1', explode_outer('item0.item')) \
     .withColumn('item2', explode_outer('item1.item')) \
     .withColumn('item3', explode_outer('item2.item')) \
     .withColumn('item4', explode_outer('item3.item')) \
     .withColumn('item5', explode_outer('item4.item')) \
-    .select(col('id'), col('item0.linkid'), col('item0.text'),  col('item1.linkid'), col('item1.text'), col('item2.linkid'), col('item3.linkid'), col('item4.linkid'), col('item5.linkid'))
-
-display(item_ds) 
+    .select(col('id'),
+            col('item0.linkid').alias('linkid_0'), col('item0.text').alias('text_0'),
+            col('item1.linkid').alias('linkid_1'), col('item1.text').alias('text_1'), 
+            col('item2.linkid').alias('linkid_2'), col('item3.linkid').alias('linkid_3'),
+            col('item4.linkid').alias('linkid_4'), col('item5.linkid').alias('linkid_5')
+    )
+display(item_df) 
 
 # COMMAND ----------
 
-response_ds = ds.read('QuestionnaireResponse').cache()
-ritem_ds = response_ds \
+#
+# Extract selected responses to the `f201` questionnaire using 
+# SQL (pyspark dataset API).
+#
+# The selected response items:
+# - linkid: 2.1 -> gender
+# - linkid: 2.3 -> country_of_birth
+# - linkid: 2.4 -> marital_status
+# - linkid: 3.1 -> smoker
+# 
+# NOTE: subject.reference can be used to join the responses with Patient resource.
+# 
+
+# 
+# Obtain the data frame for `Questionnaire` resource.
+#  
+response_df = fhir_ds.read('QuestionnaireResponse')
+
+
+#
+# Unnest the responses and project required values
+#
+response_item_df = response_df \
     .where(col('id') == 'f201') \
     .withColumn('item0', explode_outer('item')) \
     .withColumn('item1', explode_outer('item0.item')) \
     .withColumn('item2', explode_outer('item1.item')) \
-    .select(col('id'), col('subject.reference').alias('subject'), col('item0.linkid').alias('link_id0'), col('item1.linkid').alias('link_id1'), col('item1.text'), col('item1.answer.valueString').getItem(0).alias('answer'))
-display(ritem_ds) 
+    .select(col('id'), col('subject.reference').alias('subject'), 
+            col('item1.linkid').alias('linkid_1'), 
+            col('item1.answer.valueString').getItem(0).alias('answer_1'))
+display(response_item_df) 
 
 # COMMAND ----------
 
-fitems = ritem_ds.groupBy(col('id'), col('subject')).pivot("link_id1", ['2.1', '2.3', '2.4', '3.1']).agg(first('answer').alias('answer')) \
+#
+# Use `pivot()` rearrange the data frame to the required form. 
+#
+f201_responses_df = response_item_df.groupBy(col('id'), col('subject')) \
+    .pivot("linkid_1", ['2.1', '2.3', '2.4', '3.1']).agg(first('answer_1').alias('answer')) \
     .withColumnRenamed('2.1', 'gender') \
     .withColumnRenamed('2.3', 'country_of_birth') \
     .withColumnRenamed('2.4', 'marital_status') \
     .withColumnRenamed('3.1', 'smoker') 
-display(fitems)
+display(f201_responses_df)
 
 # COMMAND ----------
 
-x = ds.extract('QuestionnaireResponse', 
+#
+# Extract selected responses to the `f201` questionnaire using 
+# fhirpath and the `extract()` operation.
+#
+# The selected response items:
+# - linkid: 2.1 -> gender
+# - linkid: 2.3 -> country_of_birth
+# - linkid: 2.4 -> marital_status
+# - linkid: 3.1 -> smoker
+# 
+# NOTE: subject.reference can be used to join the responses with Patient resource.
+#  
+f201_responses_df = fhir_ds.extract('QuestionnaireResponse', 
     [
         'id',
-        fpe("subject.reference").alias("subject"),
-        fpe("item.item.where(linkId='2.1').answer.valueString.first()").alias("gender"), 
-        fpe("item.item.where(linkId='2.3').answer.valueString.first()").alias("country_of_birth"), 
-        fpe("item.item.where(linkId='2.4').answer.valueString.first()").alias("maritial_status"), 
-        fpe("item.item.where(linkId='3.1').answer.valueString.first()").alias("smoker"), 
+        exp("subject.reference").alias("subject"),
+        exp("item.item.where(linkId='2.1').answer.valueString.first()").alias("gender"), 
+        exp("item.item.where(linkId='2.3').answer.valueString.first()").alias("country_of_birth"), 
+        exp("item.item.where(linkId='2.4').answer.valueString.first()").alias("maritial_status"), 
+        exp("item.item.where(linkId='3.1').answer.valueString.first()").alias("smoker"), 
     ], 
     filters=["id = 'f201'"])
-display(x)
+    
+display(f201_responses_df)
