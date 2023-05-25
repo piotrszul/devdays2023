8ee4efd2a23ea7f0ce5b40014f447567c86498c1
piotrszul <piotrszul>
2023-05-25 01:27:58 +0000

    Finalised the ExtensionDemo and added NDJON encoded sample data to s3.

--- etc/HandlingExtensionsDemo.py
+++ etc/HandlingExtensionsDemo.py
@@ -1,59 +1,80 @@
 # Databricks notebook source
-# MAGIC %md
-# MAGIC
-# MAGIC Extensions on `Patient` resource:
-# MAGIC
-# MAGIC - string: http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName
-# MAGIC - Address: http://hl7.org/fhir/StructureDefinition/patient-birthPlace
-# MAGIC - decimal: http://synthetichealth.github.io/synthea/quality-adjusted-life-years 
-# MAGIC - decimal: http://synthetichealth.github.io/synthea/disability-adjusted-life-years
-# MAGIC
-# MAGIC
-# MAGIC Extensions on `address` element:
-# MAGIC
-# MAGIC - complex: http://hl7.org/fhir/StructureDefinition/geolocation
-# MAGIC     - decimal: latitude
-# MAGIC     - decimal: longitude
-# MAGIC
-# MAGIC
-# MAGIC
-# MAGIC Select the `quality_adjusted_life_years`, `address_latitude`, `address_longitude` for each Patient, using both SQL and `extract` operation.
 
-# COMMAND ----------
+"""
+Demonstrates how to use SQL and fhirpath with the Pathling `extract()` operation
+to obtain extension values.
+
+
+Extensions definf on `Patient` resource:
+
+- string: http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName
+- Address: http://hl7.org/fhir/StructureDefinition/patient-birthPlace
+- decimal: http://synthetichealth.github.io/synthea/quality-adjusted-life-years 
+- decimal: http://synthetichealth.github.io/synthea/disability-adjusted-life-years
+
+
+Extensions defined on `address` element:
+
+- complex: http://hl7.org/fhir/StructureDefinition/geolocation
+    - decimal: latitude
+    - decimal: longitude
+
+We demonstrate how select the `city_of_birth`, `quality_adjusted_life_years`, 
+`address_latitude`, `address_longitude`  values for each Patient 
+using both SQL and `extract()` operation.
+"""
+
 
 from pathling import PathlingContext
-from pathling import Expression as fpe
-from pyspark.sql.functions import col
-pc = PathlingContext.create(spark)
-spark.catalog.setCurrentDatabase('devdays_fhir')
+
+#
+# Initialize Pathling context with enabled support for extensions
+#
+pc = PathlingContext.create(spark, enable_extensions = True)
+
+#
+# Create a FHRI data source on a directory with ndjons encoded resource files.
+# 
+fhir_ds = pc.read.ndjson('s3://pathling-demo/staging/devdays/ndjson/')
+
+#
+# Obtain the data frame for the `Patient` resource and register it a temorary view
+# so that it can be used in SQL queries.
+#
+fhir_ds.read('Patient').createOrReplaceTempView('patient_with_extensions')
 
 # COMMAND ----------
 
 # MAGIC %sql
+# MAGIC --
+# MAGIC -- Select extension values using SQL 
+# MAGIC -- and direct references to the `_extension` MAP column.
+# MAGIC -- 
 # MAGIC SELECT id, 
-# MAGIC   filter(_extension[_fid], e -> e.url='http://hl7.org/fhir/StructureDefinition/patient-birthPlace').valueAddress[0].city as city_of_birth,
-# MAGIC   filter(_extension[_fid], e -> e.url='http://hl7.org/fhir/StructureDefinition/patient-birthPlace').valueAddress[0].country as country_of_birth,
+# MAGIC   filter(_extension[_fid], e -> e.url='http://hl7.org/fhir/StructureDefinition/patient-birthPlace').valueAddress[0].city AS city_of_birth,
 # MAGIC   filter(_extension[_fid], e -> e.url='http://synthetichealth.github.io/synthea/quality-adjusted-life-years').valueDecimal[0] AS quality_adjusted_life_years, 
-# MAGIC   address[0].city AS city, 
-# MAGIC   address[0].state AS state, 
-# MAGIC   address[0].country AS country, 
-# MAGIC   filter(_extension[filter(_extension[address[0]._fid], e -> e.url='http://hl7.org/fhir/StructureDefinition/geolocation')[0]._fid], e -> e.url='latitude')[0].valueDecimal AS address_latitude, 
-# MAGIC   filter(_extension[filter(_extension[address[0]._fid], e -> e.url='http://hl7.org/fhir/StructureDefinition/geolocation')[0]._fid], e -> e.url='longitude')[0].valueDecimal AS address_longitude 
-# MAGIC FROM patient 
+# MAGIC   filter(_extension[filter(_extension[address[0]._fid], e -> e.url='http://hl7.org/fhir/StructureDefinition/geolocation')[0]._fid], 
+# MAGIC         e -> e.url='latitude')[0].valueDecimal AS address_latitude, 
+# MAGIC   filter(_extension[filter(_extension[address[0]._fid], e -> e.url='http://hl7.org/fhir/StructureDefinition/geolocation')[0]._fid], 
+# MAGIC         e -> e.url='longitude')[0].valueDecimal AS address_longitude 
+# MAGIC FROM patient_with_extensions 
 # MAGIC ORDER BY id;
 
 # COMMAND ----------
 
-ds = pc.read.tables()
-result = ds.extract('Patient', [
-    fpe("id"), 
-    fpe("extension('http://hl7.org/fhir/StructureDefinition/patient-birthPlace').valueAddress.city.first()").alias("city_of_birth"),
-    fpe("extension('http://hl7.org/fhir/StructureDefinition/patient-birthPlace').valueAddress.country.first()").alias("country_of_birth"),
-    fpe("extension('http://synthetichealth.github.io/synthea/quality-adjusted-life-years').valueDecimal").alias("quality_adjusted_life_years"),
-    fpe("address.city.first()", "city"),
-    fpe("address.state.first()", "state"),
-    fpe("address.country.first()", "country"),
-    fpe("address.extension('http://hl7.org/fhir/StructureDefinition/geolocation').extension('latitude').valueDecimal.first()").alias("address_latitude"),
-    fpe("address.extension('http://hl7.org/fhir/StructureDefinition/geolocation').extension('longitude').valueDecimal.first()").alias("address_longitude"),
+from pathling import Expression as exp
+
+#
+# Select extension values using `extract()` operation 
+# and the fhirpath `extension()` function.
+#
+
+result = fhir_ds.extract('Patient', [
+    exp("id"), 
+    exp("extension('http://hl7.org/fhir/StructureDefinition/patient-birthPlace').valueAddress.city.first()").alias("city_of_birth"),
+    exp("extension('http://synthetichealth.github.io/synthea/quality-adjusted-life-years').valueDecimal").alias("quality_adjusted_life_years"),
+    exp("address.extension('http://hl7.org/fhir/StructureDefinition/geolocation').extension('latitude').valueDecimal.first()").alias("address_latitude"),
+    exp("address.extension('http://hl7.org/fhir/StructureDefinition/geolocation').extension('longitude').valueDecimal.first()").alias("address_longitude"),
 ])
+
 display(result.orderBy('id'))
